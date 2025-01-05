410bb9e78c7959a2fd287cc9798b6957227392b8
uttamkn <uttamkn15@gmail.com>
2025-01-06 00:08:09 +0530

    fix: add adminSecret to NhostClient configuration

--- lib/nhost.ts
+++ lib/nhost.ts
@@ -1,8 +1,9 @@
-import { NhostClient } from '@nhost/nextjs'
+import { NhostClient } from "@nhost/nextjs";
 
 const nhost = new NhostClient({
   subdomain: process.env.NEXT_PUBLIC_NHOST_SUBDOMAIN as string,
   region: process.env.NEXT_PUBLIC_NHOST_REGION as string,
-})
+  adminSecret: process.env.NEXT_PUBLIC_NHOST_ADMIN_SECRET as string,
+});
 
-export { nhost }
+export { nhost };
