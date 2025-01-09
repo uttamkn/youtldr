d4786fbf35d6ba6cf4d12b591fc01ac545f7f6e1
uttamkn <uttamkn15@gmail.com>
2025-01-09 11:35:07 +0530

    fix: remove admin key from the client

--- lib/nhost.ts
+++ lib/nhost.ts
@@ -3,7 +3,6 @@ import { NhostClient } from "@nhost/nextjs";
 const nhost = new NhostClient({
   subdomain: process.env.NEXT_PUBLIC_NHOST_SUBDOMAIN as string,
   region: process.env.NEXT_PUBLIC_NHOST_REGION as string,
-  adminSecret: process.env.NEXT_PUBLIC_NHOST_ADMIN_SECRET as string,
 });
 
 export { nhost };
